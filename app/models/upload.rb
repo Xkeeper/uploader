b8a84c360f2539b1aaf1871d20b516016033b71f
Xkeeper <xkeepers@gmail.com>
2013-02-07 14:35:04 +0400

    randomize upload id

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,10 +1,16 @@
 class Upload < ActiveRecord::Base
   attr_accessible :filename, :filesize, :file_path, :file_url
-  before_create :set_date
+  before_create :set_date, :randomize_id
   before_destroy :cleanup
   validates :filename, :file_path, :presence => true
 
 
+private
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(10000000)
+    end while Upload.where(:id => self.id).exists?
+  end
   def set_date
   	self.upload_date = DateTime.now()
   end
